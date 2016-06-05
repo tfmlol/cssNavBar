5d35b55216c59b55d8ac1f043ebc50bce8849df7
tfmlol <tfm2lol@gmail.com>
2016-06-05 19:08:13 -0400

    navBar V1.0

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function dropDown() {
+   document.getElementByClassName('topNav')[0].classList.toggle("responsive");
+}
+
+function collectSouls() {
+   document.getElementByClassName("souls").onclick.alert("Soul Collected");
+}
